fd3e55447db47eb854df7d9df868dd99fd86052e
Kharsv <Kharsv@gmail.com>
2022-05-20 23:16:12 +0300

    Add d04

---
+++ d04/ex01/my_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+python3 -m venv venv
+source venv/bin/activate
+pip --version
+pip install -r requirement.txt
+python3 -m django startproject d04
+cd d04
+python3 -m django startapp ex01
+python manage.py runserver
