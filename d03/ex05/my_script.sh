2c39ec24a7279173b67165b77ba73ada0fda7d28
Kharsv <Kharsv@gmail.com>
2022-05-20 20:29:11 +0300

    Fix d03 ex05

--- d03/ex05/my_script.sh
+++ d03/ex05/my_script.sh
@@ -4,7 +4,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip --version
 pip install -r requirement.txt
-django-admin startproject hello
-cd hello
-django-admin startapp helloworld
-python manage.py runserver
+python3 -m django startproject helloproject
+cd helloproject
+python3 -m django startapp helloworld
+python manage.py runserverls
